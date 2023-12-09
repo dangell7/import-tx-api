8dc40d39705cc1b1751b958e0e213316e7e082b4
Denis Angell <dangell@transia.co>
2023-12-09 20:06:10 +0100

    remove old files

--- server/swagger/latest.sh
+++ server/swagger/latest.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-swagger-codegen generate -c config-py.json -i latest.yaml -l python-flask -o ../.. -Dmodels
+swagger-codegen generate -c config.json -i latest.yaml -l python-flask -o ../.. -Dmodels
